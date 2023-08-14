2e7da8abc38686d0e163e75373dda47fd3ce81c5
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-08-15 00:16:55 +0200

    Refactor /create route to a functional status

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -7,29 +7,14 @@ router.get("/", (req, res) => {
   res.json("All good in here");
 });
 
-router.post("/create", (req, res) => {
-  console.log(req.body);
-  const { url, name, year, description, technique, size, sale } = req.body;
-  console.log(url, name, year, description);
-  res.json("Entry created");
+router.post("/create", async (req, res) => {
+  try {
+    const newItem = await Art.create(req.body);
+    const response = { data: newItem, message: "A new entry has been added to the DB" };
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 });
-
-//
-// router.post("/", async (req, res) => {
-//   const test = {
-//     url: "www.google.com",
-//     name: "Monna Lisa",
-//     year: 2021,
-//     description: "Pretty picture",
-//     technique: "acryl leinwand",
-//     size: { height: 30, width: 40 },
-//     sale: [
-//       { format: "poster", price: 40 },
-//       { format: "original", price: 300 },
-//     ],
-//   };
-//   const created = await Art.create(test);
-//   res.json("Entry created");
-// });
 
 module.exports = router;
